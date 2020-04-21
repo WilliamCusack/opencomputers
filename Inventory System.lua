f0466ef61ed032d26c5960c7e1496169af274e81
WilliamCusack <williamfcusack@gmail.com>
2020-04-21 13:16:16 -0600

    initial commit

---
+++ Inventory System.lua
@@ -0,0 +1,87 @@
+print("IM is running!")
+--[[local c = require("component")
+local ct = c.transposer
+local shell = require("shell")
+local sides = require("sides")
+
+---- Config Variables 
+local recipeSide = sides.east
+local outputSide = sides.down
+local parserItem = nil      --can set these two manually if you want
+local parserEndItem = nil   --^^^^
+
+---------------------------------------------------------------
+
+
+--Config Functions to find parser blocks and whatnot
+    --find parser item, find parser end item
+    --will be on a chest on top of the transposer with 3/3 parser/end blocks
+    
+setParserItem()
+
+
+setParserEndItem()
+
+
+--## Storage System ##--
+print("IM is running!")
+
+
+---- Empty Input Chest---  Basically starting from a fresh state.
+
+---- Check Storage and Create a Table of it ----
+
+---- Send a Call for recipe Tables----
+
+---- Recieve Crafting Recipe Tables ----
+
+
+
+
+--## Crafting System ##--
+
+
+
+
+----Check all transposers with the parser block in first slot and
+    --end block in them
+
+
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------------------------
+-----------FUNCTIONS------------------------------------
+--------------------------------------------------------
+
+local function setParserItem()
+    if parserItem then
+        local transposers = findComponents("ransposer")
+
+        
+    end
+end
+
+local function setParserEndItem()
+    if parserEndItem then
+    
+    end
+end
+
+local function findComponents(componentName)     --Makes a table of all the address of components of the name
+    local components = {}
+    for address, name in c.list(componentName, false) do
+        table.insert(components, c.list(address))
+
+    end
+    return components
+end
+--]]
---
+++ starter.lua
@@ -0,0 +1,3 @@
+print("Hello! Starting the download and run of the ivm.lua script")
+shell.execute("pastebin get -f BB91UYnf ivm.lua")
+dofile("ivm.lua")
