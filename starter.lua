f847632c000fbc397697fbaf8eaae45ac5a1a5a7
WilliamCusack <williamfcusack@gmail.com>
2020-04-21 20:27:37 -0600

    moved things to wget and git will only have to do this from now on again again

--- starter.lua
+++ starter.lua
@@ -1,5 +1,5 @@
 local shell = require("shell")
 
 print("Hello! Starting the download and run of the ivm.lua script")
-shell.execute("wget -f https://raw.githubusercontent.com/WilliamCusack/opencomputers/master/starter.lua ivm.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/WilliamCusack/opencomputers/master/Inventory%20System.lua ivm.lua")
 dofile("ivm.lua")
