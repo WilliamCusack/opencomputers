ac46d5793386fced5fdb65c17ee2b8a12e56a83b
WilliamCusack <williamfcusack@gmail.com>
2020-05-05 15:54:33 -0600

    updating that starter file to maybe replace itself

--- starter.lua
+++ starter.lua
@@ -1,7 +1,14 @@
 local shell = require("shell")
 
-print("Hello! Starting the download and run of the ivm.lua script")
-shell.execute("wget -f --no-check-certificate --content-disposition https://www.dropbox.com/s/l31qc2gjdqb3waj/InventorySystem.lua?dl=1 ivm.lua")
+local args, opts = shell.parse(...)
+
+if args[1] = "dl" then
+	print("Hello! Starting the download and run of the ivm.lua script")
+	shell.execute("wget -f --no-check-certificate --content-disposition https://www.dropbox.com/s/l31qc2gjdqb3waj/InventorySystem.lua?dl=1 ivm.lua")
+end
+if args[1] = "fresh" then
+	print("Hello, we're gonna try to replace this starter file with a new one. Wish me luck!")
+end
 dofile("ivm.lua")
 
 -------------------######
